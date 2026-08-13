3e9a629dd35061ea6a2ea9adf1bb8f5aebde001d
Felipe Diaz <pipediazpe@gmail.com>
2026-08-12 19:37:17 -0600

    Registro e inicio de sesion con Firebase

--- src/App.jsx
+++ src/App.jsx
@@ -3,26 +3,45 @@ import { useState } from "react"
 
 // Importamos las páginas principales
 import { Login } from "./pages/Login"
+import { Register } from "./pages/Register"
 import { Home } from "./pages/Home"
 
 
 export const App = () => {
 
-    // Esta variable guarda si el usuario inició sesión.
-    // Al principio está en false porque todavía no ha ingresado.
+    // Indica si el usuario ya inició sesión
     const [usuarioActivo, setUsuarioActivo] = useState(false)
 
+    // Controla si mostramos Login o Registro
+    const [pantalla, setPantalla] = useState("login")
 
-    // Si el usuario todavía no inició sesión, mostramos Login.
+
+    // Si el usuario todavía no inició sesión
     if (!usuarioActivo) {
+
+        // Mostramos Crear cuenta
+        if (pantalla === "registro") {
+
+            return (
+                <Register
+                    setPantalla={setPantalla}
+                />
+            )
+        }
+
+
+        // Por defecto mostramos Login
         return (
-            <Login setUsuarioActivo={setUsuarioActivo} />
+            <Login
+                setUsuarioActivo={setUsuarioActivo}
+                setPantalla={setPantalla}
+            />
         )
     }
 
 
-    // Cuando inicia sesión, mostramos la pantalla principal.
+    // Solo mostramos Home después de iniciar sesión correctamente
     return (
-        <Home />
+        <Home setUsuarioActivo={setUsuarioActivo} />
     )
 }
